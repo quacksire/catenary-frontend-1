e994b3514da31d1d63453e5db9cb3f25f78c2908
Kyler Chin <kyler@catenarymaps.org>
2026-01-09 20:25:09 -0800

    check for osm station redirection

--- src/components/StopScreen.svelte
+++ src/components/StopScreen.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import GenericStopScreen from './GenericStopScreen.svelte';
+	import { data_stack_store } from '../globalstores';
+	import { StackInterface, OsmStationStack } from './stackenum';
+
 	export let chateau: string;
 	export let stop_id: string;
 
@@ -14,6 +17,35 @@
 		});
 		return `${base}?${params.toString()}`;
 	};
+
+	const checkOsmStation = async () => {
+		try {
+			const res = await fetch(
+				`https://birch.catenarymaps.org/osm_station_lookup?chateau_id=${chateau}&gtfs_stop_id=${stop_id}`
+			);
+			const data = await res.json();
+			if (data.found && data.osm_station_id) {
+				data_stack_store.update((stack) => {
+					const newStackItem = new StackInterface(
+						new OsmStationStack(
+							String(data.osm_station_id),
+							data.osm_station_info?.name || null,
+							data.osm_station_info?.mode_type || null
+						)
+					);
+					stack.pop();
+					stack.push(newStackItem);
+					return stack;
+				});
+			}
+		} catch (e) {
+			console.error('Error checking for OSM station', e);
+		}
+	};
+
+	$: if (chateau && stop_id) {
+		checkOsmStation();
+	}
 </script>
 
 <GenericStopScreen {buildUrl} key={stop_id} />
